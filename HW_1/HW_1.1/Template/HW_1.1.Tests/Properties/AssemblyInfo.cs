e0a1b3e8d0173a3ccfd4ecfb68dd4b12bbd315c0
Agesyne <A40gE20Low8@mail.ru>
2019-03-18 21:39:58 +0300

    HW_1.1

---
+++ HW_1/HW_1.1/Template/HW_1.1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HW_1.1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW_1.1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a7329dc-b262-4e04-ba46-fc50d958a8ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HW_1/HW_1.1/Template/Template/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Template
+{
+    public class Program
+    {
+        /// <summary>
+        /// The method prevents closing console when everething is done
+        /// </summary>
+        static void HoldConsoleOn()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+        }
+
+        /// <summary>
+        /// The method counts factorial number
+        /// </summary>
+        /// <param name="number"> N-th factorial number</param>
+        /// <returns></returns>
+        public static int CountFactorial(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int result = 1;
+
+            for (int i = 1; i <= number; ++i)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Check if number entered wrong
+        /// </summary>
+        /// <param name="enteredString"> User input</param>
+        /// <param name="number"> Read number from the string</param>
+        /// <returns></returns>
+        public static int isNumberEntered(string enteredString, out int number)
+        {
+            bool isNumberic = Int32.TryParse(enteredString, out number);
+            if (!isNumberic)
+            {
+                throw new ArgumentOutOfRangeException("String is not a number");
+            }
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program counts Factorial");
+            Console.Write("Enter number: ");
+            string enteredString = Console.ReadLine();
+
+            
+            try
+            {
+                isNumberEntered(enteredString, out int number);
+                Console.WriteLine($"{number}-th number is {CountFactorial(number)}");
+            }
+            catch (ArgumentOutOfRangeException e) when (e.ParamName == "String is not a number")
+            {
+                Console.WriteLine($"\"{enteredString}\" is not a number");
+            }
+            catch (ArgumentOutOfRangeException e) when (e.ParamName == "Number <=0")
+            {
+                Console.WriteLine("Number can't be <= 0");
+            }
+
+            HoldConsoleOn();
+        }
+    }
+}
