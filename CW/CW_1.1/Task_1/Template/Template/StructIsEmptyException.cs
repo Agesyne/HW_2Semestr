6f0032ee2c85dd0743d0670edaa21e5ff1681aab
Agesyne <A40gE20Low8@mail.ru>
2019-04-12 15:15:56 +0300

    CW_1.1

---
+++ CW/CW_1.1/Task_1/Template/Template/StructIsEmptyException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Template
+{
+    /// <summary>
+    /// Special exception to throw if struct is empty
+    /// </summary>
+    [Serializable]
+    public class StructIsEmptyException : Exception
+    {
+        public StructIsEmptyException() { }
+        public StructIsEmptyException(string message, Exception inner) 
+            : base(message, inner)
+        {
+            Console.WriteLine("Struct is empty => extracting is impossible.");
+        }
+        protected StructIsEmptyException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+                : base(info, context) { }
+    }
+}
