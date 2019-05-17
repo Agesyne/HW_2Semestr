0255093938352c020fdd19284d5fbd9ad943ef5a
Agesyne <A40gE20Low8@mail.ru>
2019-05-17 15:21:05 +0300

    CW_3.2

---
+++ CW/CW_3.2/Task_1/CW_3.2_1/CW_3.2_1/SomeSortingClassComparerByNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class SomeSortingClassComparerByNumber : IComparer<SomeSortingClass>
+    {
+        public int Compare(SomeSortingClass a, SomeSortingClass b)
+        {
+            return String.Compare(a.Number, b.Number);
+        }
+    }
+}
---
+++ CW/CW_3.2/Task_1/CW_3.2_1/Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestIntSorting()
+        {
+            List<int> checkingList = new List<int>();
+            int[] checkingArray = new int[] { 3, 8, 19, -5, 23, 100, 23, -5, 0, -7, 3 };
+            foreach (var i in checkingArray)
+            {
+                checkingList.Add(i);
+            }
+
+            Sorting.BubbleSort<int>.Sort(checkingList, new StandartComparer());
+            Assert.IsTrue(Sorting.BubbleSort<int>.IsSorted(checkingList, new StandartComparer()));
+        }
+
+        [TestMethod]
+        public void TestSomeClassSorting()
+        {
+            List<SomeSortingClass> checkingList = new List<SomeSortingClass>();
+            string[] numbers = new string[] { "123", "234", "012", "500" };
+            foreach (var i in numbers)
+            {
+                var newElement = new SomeSortingClass(1, "a", i);
+                checkingList.Add(newElement);
+            }
+            Sorting.BubbleSort<SomeSortingClass>.Sort(checkingList, new SomeSortingClassComparerByNumber());
+            Assert.IsTrue(Sorting.BubbleSort<SomeSortingClass>.IsSorted(checkingList, new SomeSortingClassComparerByNumber()));
+        }
+    }
+}
