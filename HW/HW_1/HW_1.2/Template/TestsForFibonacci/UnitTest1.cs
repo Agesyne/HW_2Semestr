9e07da70001577fffbbbeeddd1ec2223ed0f8548
Agesyne <A40gE20Low8@mail.ru>
2019-05-25 10:49:21 +0300

    HW_1.2

---
+++ HW/HW_1/HW_1.2/Template/TestsForFibonacci/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsForFibonacci
+{
+    /// <summary>
+    /// Test Fibonacci
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Test Fibonacci method working
+        /// </summary>
+        [TestMethod]
+        public void TestFibonacci()
+        {
+            int[] sourse = { 0, 1, 2, 3, 7, 10, 15, -1, -100 };
+            int[] answers = { 0, 1, 1, 2, 13, 55, 610 };
+
+            bool isAllRight = true;
+            for (var i = 0; i < answers.Length; ++i)
+            {
+                if (MathFibonacci.Program.GetNthFibonacciNumber(sourse[i]) != answers[i])
+                {
+                    isAllRight = false;
+                    break;
+                }
+            }
+            Assert.IsTrue(isAllRight);
+            for (var i = answers.Length; i < sourse.Length; ++i)
+            {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => MathFibonacci.Program.GetNthFibonacciNumber(sourse[i]));
+            }
+        }
+    }
+}
