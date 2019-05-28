012c7d3dc722e947a0c65df68934a2c19d36c142
Agesyne <A40gE20Low8@mail.ru>
2019-05-28 16:00:38 +0300

    Temp HW_2.2

---
+++ HW/HW_2/HW_2.2/Template/Template/HashMap.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Structures
+{
+    /// <summary>
+    /// ADS HashMap
+    /// </summary>
+    public class HashMap
+    {
+        /// <summary>
+        /// The storage of values
+        /// </summary>
+        private LinkedList[] map;
+
+        /// <summary>
+        /// Get hashcode by value
+        /// </summary>
+        /// <param name="value">The given value</param>
+        /// <returns></returns>
+        private int GetHash(int value)
+        {
+            return Math.Abs(value) % map.Length;
+        }
+
+        /// <summary>
+        /// Add value in hashtable
+        /// </summary>
+        /// <param name="value">The adding value</param>
+        public void Add(int value)
+        {
+            int key = GetHash(value);
+            map[key].Add(value, 0);
+        }
+
+        /// <summary>
+        /// Delete value from hashtable
+        /// </summary>
+        /// <param name="value">The deleting value</param>
+        public void Delete(int value)
+        {
+            int key = GetHash(value);
+            int cellLength = map[key].Count;
+            for (var i = 0; i < cellLength; ++i)
+            {
+                if (map[key].GetNthValue(i) == value)
+                {
+                    map[key].Delete(i);
+                    return;
+                }
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        /// <summary>
+        /// Check if a value exist in hashtable
+        /// </summary>
+        /// <param name="value">The given value</param>
+        /// <returns></returns>
+        public bool Exist(int value)
+        {
+            int key = GetHash(value);
+            int cellLength = map[key].Count;
+            for (var i = 0; i < cellLength; ++i)
+            {
+                if (map[key].GetNthValue(i) == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Constactor with choice of internal storage length
+        /// </summary>
+        /// <param name="length">The wanted length</param>
+        public HashMap(int length = 8)
+        {
+            map = new LinkedList[length];
+            for (var i = 0; i < length; ++i)
+            {
+                map[i] = new LinkedList();
+            }
+        }
+    }
+}
---
+++ HW/HW_2/HW_2.2/Template/Tests/TestHashTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Structures;
+
+namespace Tests
+{
+    /// <summary>
+    /// Test HashTable class
+    /// </summary>
+    [TestClass]
+    public class TestHashTable
+    {
+        private void AddSomeValues(HashMap map)
+
+        /// <summary>
+        /// Test Add method
+        /// </summary>
+        [TestMethod]
+        public void TestAddMethod()
+        {
+            var map = new HashMap();
+
+            map.Add(0);
+            map.Add(4);
+            map.Add(3);
+            map.Add(1);
+            map.Add(2);
+        }
+    }
+}
