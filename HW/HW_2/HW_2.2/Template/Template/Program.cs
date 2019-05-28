8cb2affe17f2bbffaf1df984a0f0e7495aeda1dd
Agesyne <A40gE20Low8@mail.ru>
2019-05-28 16:34:57 +0300

    HW_2.1

--- HW/HW_2/HW_2.2/Template/Template/Program.cs
+++ HW/HW_2/HW_2.2/Template/Template/Program.cs
@@ -1,33 +1,11 @@
-﻿using System;
+﻿
 
-namespace Template
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var map = new Structures.HashMap();
-            map.Add(0);
-            map.Add(4);
-            map.Add(3);
-            map.Add(1);
-            map.Add(2);
-            Console.WriteLine(map.Exist(4));
-            Console.WriteLine(map.Exist(0));
-            Console.WriteLine(map.Exist(1));
-            Console.WriteLine(map.Exist(-1));
-            Console.WriteLine(map.Exist(5));
-            Console.WriteLine(map.Exist(16));
-            Console.WriteLine(map.Exist(14));
-            Console.WriteLine();
-            map.Delete(4);
-            map.Delete(1);
-            map.Delete(0);
-            Console.WriteLine(map.Exist(4));
-            Console.WriteLine(map.Exist(1));
-            Console.WriteLine(map.Exist(0));
-            Console.ReadKey();
-
         }
     }
 }
