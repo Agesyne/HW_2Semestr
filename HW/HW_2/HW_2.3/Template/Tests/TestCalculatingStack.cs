02c607213d1588f7117eabe4b00907582df30f55
Agesyne <A40gE20Low8@mail.ru>
2019-05-29 23:26:51 +0300

    Fixed tests

---
+++ HW/HW_2/HW_2.3/Template/Tests/TestCalculatingStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatingStack;
+
+namespace Tests
+{
+    /// <summary>
+    /// Test StackCalculator class
+    /// </summary>
+    [TestClass]
+    public class TestStackCalculator
+    {
+        /// <summary>
+        /// Test CalculatePosfixExpression method
+        /// </summary>
+        [TestMethod]
+        public void TestCalculatePostfixExpression()
+        {
+            string[] expressions = {
+                "89+ 2- 2* 6/",
+                "11+ 1+ 1+ 1+ 1+ 5- 0+",
+                "01- 5- 6+ 5*",
+                "33/ 9* 3/ 6* 6/",
+                "81+ 9- 5+ 9- 8+ 0-",
+                "89+ 2- 2* 6/ 0*"
+                                    };
+
+            int[] results = { 5, 1, 0, 3, 4, 0 };
+
+            for (var i = 0; i < expressions.Length; ++i)
+            {
+                Assert.AreEqual(results[i], StackCalculator.CalculatePostfixExpression(expressions[i], new StackByList()));
+                Assert.AreEqual(results[i], StackCalculator.CalculatePostfixExpression(expressions[i], new StackByArray()));
+            }
+        }
+
+        /// <summary>
+        /// Test expressions
+        /// </summary>
+        [TestMethod]
+        public void TestExceptions()
+        {
+            string expression1 = "23+4";
+            string expression2 = "23-4+#";
+            string expression3 = "25+";
+            string expression4 = null;
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => StackCalculator.CalculatePostfixExpression(expression1, new StackByList()));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => StackCalculator.CalculatePostfixExpression(expression2, new StackByList()));
+            StackByList newStack = null;
+            Assert.ThrowsException<ArgumentNullException>(() => StackCalculator.CalculatePostfixExpression(expression3, newStack));
+            Assert.ThrowsException<ArgumentNullException>(() => StackCalculator.CalculatePostfixExpression(expression4, new StackByList()));
+        }
+    }
+}
---
+++ HW/HW_2/HW_2.3/Template/Tests/TestStackInterfaceImplementations.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatingStack;
+
+namespace Tests
+{
+    /// <summary>
+    /// Test IADSStack
+    /// </summary>
+    [TestClass]
+    public class TestStackInterfaceImplementations
+    {
+        /// <summary>
+        /// Push some values to stack
+        /// </summary>
+        /// <param name="stack">The given stack</param>
+        private void PushSomeValues(IADSStack stack)
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(5);
+        }
+
+        /// <summary>
+        /// Test Push method
+        /// </summary>
+        [TestMethod]
+        public void TestPushMethod()
+        {
+            IADSStack stack1 = new StackByList();
+            IADSStack stack2 = new StackByArray();
+
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Push(5);
+
+            stack2.Push(1);
+            stack2.Push(2);
+            stack2.Push(3);
+            stack2.Push(5);
+        }
+
+        /// <summary>
+        /// Test Pop method
+        /// </summary>
+        [TestMethod]
+        public void TestPopMethod()
+        {
+            IADSStack stack1 = new StackByList();
+            IADSStack stack2 = new StackByArray();
+
+            PushSomeValues(stack1);
+            PushSomeValues(stack2);
+
+            int[] results = { 5, 3, 2, 1 };
+            for (var i = 0; i < stack1.Count; ++i)
+            {
+                Assert.AreEqual(stack1.Pop(), results[i]);
+            }
+            for (var i = 0; i < stack2.Count; ++i)
+            {
+                Assert.AreEqual(stack2.Pop(), results[i]);
+            }
+        }
+
+        /// <summary>
+        /// Test Peek method
+        /// </summary>
+        [TestMethod]
+        public void TestPeekMethod()
+        {
+            IADSStack stack1 = new StackByList();
+            IADSStack stack2 = new StackByArray();
+
+            PushSomeValues(stack1);
+            PushSomeValues(stack2);
+
+            int[] results = { 5, 3, 2, 1 };
+            for (var i = 0; i < stack1.Count; ++i)
+            {
+                Assert.AreEqual(stack1.Peek(), results[i]);
+                stack1.Pop();
+            }
+            for (var i = 0; i < stack2.Count; ++i)
+            {
+                Assert.AreEqual(stack2.Peek(), results[i]);
+                stack2.Pop();
+            }
+        }
+
+        /// <summary>
+        /// Test IsEmpty method
+        /// </summary>
+        [TestMethod]
+        public void TestIsEmptyMethod()
+        {
+            IADSStack stack1 = new StackByList();
+            IADSStack stack2 = new StackByArray();
+
+            Assert.IsTrue(stack1.IsEmpty());
+            stack1.Push(1);
+            Assert.IsFalse(stack1.IsEmpty());
+            stack1.Pop();
+            Assert.IsTrue(stack1.IsEmpty());
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Pop();
+            Assert.IsFalse(stack1.IsEmpty());
+            stack1.Pop();
+            Assert.IsTrue(stack1.IsEmpty());
+
+            Assert.IsTrue(stack2.IsEmpty());
+            stack2.Push(1);
+            Assert.IsFalse(stack2.IsEmpty());
+            stack2.Pop();
+            Assert.IsTrue(stack2.IsEmpty());
+            stack2.Push(2);
+            stack2.Push(3);
+            stack2.Pop();
+            Assert.IsFalse(stack2.IsEmpty());
+            stack2.Pop();
+            Assert.IsTrue(stack2.IsEmpty());
+        }
+
+        /// <summary>
+        /// Test Count property
+        /// </summary>
+        [TestMethod]
+        public void TestCountProperty()
+        {
+            IADSStack stack1 = new StackByList();
+            IADSStack stack2 = new StackByArray();
+
+            int[] results = { 4, 3, 4, 3, 1, 0, 1, 2, 0 };
+            int resultCounter = 0;
+
+            PushSomeValues(stack1);
+            PushSomeValues(stack2);
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Pop();
+            stack2.Pop();
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Push(4);
+            stack2.Push(4);
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Pop();
+            stack2.Pop();
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Pop();
+            stack1.Pop();
+            stack2.Pop();
+            stack2.Pop();
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Pop();
+            stack2.Pop();
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Push(1);
+            stack2.Push(1);
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Push(2);
+            stack2.Push(2);
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+            ++resultCounter;
+
+            stack1.Pop();
+            stack1.Pop();
+            stack2.Pop();
+            stack2.Pop();
+            Assert.AreEqual(stack1.Count, results[resultCounter]);
+            Assert.AreEqual(stack2.Count, results[resultCounter]);
+        }
+
+        /// <summary>
+        /// Test exceptions
+        /// </summary>
+        [TestMethod]
+        public void TestExceptions()
+        {
+            IADSStack stack1 = new StackByList();
+            IADSStack stack2 = new StackByArray();
+
+            Assert.ThrowsException<InvalidOperationException>(() => stack1.Pop());
+            Assert.ThrowsException<InvalidOperationException>(() => stack1.Peek());
+
+            Assert.ThrowsException<InvalidOperationException>(() => stack2.Pop());
+            Assert.ThrowsException<InvalidOperationException>(() => stack2.Peek());
+        }
+    }
+}
