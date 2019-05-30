7a6d0d63afed63b81d8f4628f32ae783a1788033
Agesyne <A40gE20Low8@mail.ru>
2019-05-30 15:41:19 +0300

    HW_6.1

---
+++ HW/HW_6/HW_6.1/Template/Template/ListFunctons.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StructureFunctions
+{
+    /// <summary>
+    /// Some functions with list
+    /// </summary>
+    public class ListFunctions
+    {
+        /// <summary>
+        /// Make function to every element in given list
+        /// </summary>
+        /// <param name="list">The given list</param>
+        /// <param name="transformation">The transforming function</param>
+        /// <returns>The transformed list</returns>
+        public static List<int> Map(List<int> list, Func<int, int> transformation)
+        {
+            if (list == null || transformation == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            for (var i = 0; i < list.Count; ++i)
+            {
+                list[i] = transformation(list[i]);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Make new list with matching elements elements from given list
+        /// </summary>
+        /// <param name="list">The given list</param>
+        /// <param name="match">The matching function</param>
+        /// <returns>The matched list</returns>
+        public static List<int> Filter(List<int> list, Func<int, bool> match)
+        {
+            if (list == null || match == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var newList = new List<int>();
+            foreach (var i in list)
+            {
+                if (match(i))
+                {
+                    newList.Add(i);
+                }
+            }
+            return newList;
+        }
+
+        /// <summary>
+        /// Accumulate value from all list plus starting value
+        /// </summary>
+        /// <param name="list">The given list</param>
+        /// <param name="summator">The starting value</param>
+        /// <param name="accumulating">The accumulating function</param>
+        /// <returns>The accumulated value</returns>
+        public static int Fold(List<int> list, int summator, Func<int, int, int> accumulating)
+        {
+            if (list == null || accumulating == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            foreach (var i in list)
+            {
+                summator = accumulating(summator, i);
+            }
+            return summator;
+        }
+
+    }
+}
---
+++ HW/HW_6/HW_6.1/Template/UnitTests/TestListFunctions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using StructureFunctions;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Test ListFuntions class
+    /// </summary>
+    [TestClass]
+    public class TestListFunctions
+    {
+        /// <summary>
+        /// Compare if lists are same
+        /// </summary>
+        /// <param name="a">The 1st list</param>
+        /// <param name="b">The 2nd list</param>
+        private static bool IsSameList(List<int> a, List<int> b)
+        {
+            if (a.Count != b.Count)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < a.Count; ++i)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Test Map method
+        /// </summary>
+        [TestMethod]
+        public void TestMapMethod()
+        {
+            var list1 = new List<int>() { 1, 2, 3, 4, 5 };
+            var list2 = new List<int>() { -1, -2, -3, -4, -5 };
+            var list3 = new List<int>() { -10, 20, -30, 40, -50 };
+
+            var resultList1 = new List<int>() { 2, 4, 6, 8, 10 };
+            var resultList2 = new List<int>() { -6, -7, -8, -9, -10 };
+            var resultList3 = new List<int>() { -2, 4, -6, 8, -10 };
+
+            Assert.IsTrue(IsSameList(resultList1, ListFunctions.Map(list1, x => x * 2)));
+            Assert.IsTrue(IsSameList(resultList2, ListFunctions.Map(list2, x => x - 5)));
+            Assert.IsTrue(IsSameList(resultList3, ListFunctions.Map(list3, x => x / 5)));
+        }
+
+        /// <summary>
+        /// Test Filter method
+        /// </summary>
+        [TestMethod]
+        public void TestFilterMethod()
+        {
+            var list1 = new List<int>() { 1, 2, 3, 4, 5 };
+            var list2 = new List<int>() { -1, -2, -3, -4, -5 };
+            var list3 = new List<int>() { -10, 20, -30, 40, -50 };
+
+            var resultList1 = new List<int>() { 1, 3, 5 };
+            var resultList2 = new List<int>() { -3, -4, -5 };
+            var resultList3 = new List<int>() { 20, 40 };
+
+            Assert.IsTrue(IsSameList(resultList1, ListFunctions.Filter(list1, x => x % 2 == 1)));
+            Assert.IsTrue(IsSameList(resultList2, ListFunctions.Filter(list2, x => x < -2)));
+            Assert.IsTrue(IsSameList(resultList3, ListFunctions.Filter(list3, x => x >= 10)));
+        }
+
+        /// <summary>
+        /// Test Fold method
+        /// </summary>
+        [TestMethod]
+        public void TestFoldMethod()
+        {
+            var list1 = new List<int>() { 1, 2, 3, 4, 5 };
+            var list2 = new List<int>() { -1, -2, -3, -4, -5 };
+            var list3 = new List<int>() { -10, 20, -30, 40, -50 };
+
+            var resultList1 = 15;
+            var resultList2 = -120;
+            var resultList3 = -20;
+
+            Assert.AreEqual(resultList1, ListFunctions.Fold(list1, 0, (acc, elem) => acc + elem));
+            Assert.AreEqual(resultList2, ListFunctions.Fold(list2, 1, (acc, elem) => acc * elem));
+            Assert.AreEqual(resultList3, ListFunctions.Fold(list3, 10, (acc, elem) => acc + elem));
+        }
+
+        /// <summary>
+        /// Test exceptions
+        /// </summary>
+        [TestMethod]
+        public void TestExceptions()
+        {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> nullList = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => ListFunctions.Map(nullList, elem => 2 * elem));
+            Assert.ThrowsException<ArgumentNullException>(() => ListFunctions.Map(list, null));
+
+            Assert.ThrowsException<ArgumentNullException>(() => ListFunctions.Filter(nullList, elem => elem < 5));
+            Assert.ThrowsException<ArgumentNullException>(() => ListFunctions.Filter(list, null));
+
+            Assert.ThrowsException<ArgumentNullException>(() => ListFunctions.Fold(nullList, 0, (acc, elem) => acc + elem));
+            Assert.ThrowsException<ArgumentNullException>(() => ListFunctions.Fold(list, 0, null));
+        }
+
+    }
+}
