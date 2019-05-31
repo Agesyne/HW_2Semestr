bf648c612a6c3082a82aaf560b1ccab1bd4e1655
Agesyne <A40gE20Low8@mail.ru>
2019-05-31 15:33:55 +0300

    HW_3.2 Added comments to HashFunction interface

--- HW/HW_3/HW_3.2/Template/Template/HashFunctionInterface.cs
+++ HW/HW_3/HW_3.2/Template/Template/HashFunctionInterface.cs
@@ -1,9 +1,15 @@
-﻿
-
-namespace Structures
+﻿namespace Structures
 {
+    /// <summary>
+    /// Interface of hashFunction
+    /// </summary>
     public interface HashFunction
     {
+        /// <summary>
+        /// Get hashCode by value and max value
+        /// </summary>
+        /// <param name="value">The given value</param>
+        /// <param name="cellNumbers">The maximum output</param>
         int GetHash(int value, int cellNumbers);
     }
 }
