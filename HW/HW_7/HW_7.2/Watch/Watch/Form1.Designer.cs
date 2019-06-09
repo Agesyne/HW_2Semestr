90c94525eadb8c513a543847568507ef9922f4a4
Agesyne <A40gE20Low8@mail.ru>
2019-06-09 16:25:18 +0300

    HW_7.2

---
+++ HW/HW_7/HW_7.2/Watch/Watch/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Watch
+{
+    partial class Background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.HourLabel1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.HourLabel2 = new System.Windows.Forms.Label();
+            this.SharerLabel1 = new System.Windows.Forms.Label();
+            this.MinuteLabel1 = new System.Windows.Forms.Label();
+            this.MinuteLabel2 = new System.Windows.Forms.Label();
+            this.SharerLabel2 = new System.Windows.Forms.Label();
+            this.SecondLabel1 = new System.Windows.Forms.Label();
+            this.SecondLabel2 = new System.Windows.Forms.Label();
+            this.WatchTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // HourLabel1
+            // 
+            this.HourLabel1.AutoSize = true;
+            this.HourLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HourLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HourLabel1.Location = new System.Drawing.Point(12, 9);
+            this.HourLabel1.Name = "HourLabel1";
+            this.HourLabel1.Size = new System.Drawing.Size(24, 26);
+            this.HourLabel1.TabIndex = 1;
+            this.HourLabel1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(388, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(396, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(404, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // HourLabel2
+            // 
+            this.HourLabel2.AutoSize = true;
+            this.HourLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HourLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HourLabel2.Location = new System.Drawing.Point(42, 9);
+            this.HourLabel2.Name = "HourLabel2";
+            this.HourLabel2.Size = new System.Drawing.Size(24, 26);
+            this.HourLabel2.TabIndex = 2;
+            this.HourLabel2.Text = "0";
+            // 
+            // SharerLabel1
+            // 
+            this.SharerLabel1.AutoSize = true;
+            this.SharerLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SharerLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SharerLabel1.Location = new System.Drawing.Point(72, 9);
+            this.SharerLabel1.Name = "SharerLabel1";
+            this.SharerLabel1.Size = new System.Drawing.Size(18, 26);
+            this.SharerLabel1.TabIndex = 7;
+            this.SharerLabel1.Text = ":";
+            // 
+            // MinuteLabel1
+            // 
+            this.MinuteLabel1.AutoSize = true;
+            this.MinuteLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinuteLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinuteLabel1.Location = new System.Drawing.Point(96, 9);
+            this.MinuteLabel1.Name = "MinuteLabel1";
+            this.MinuteLabel1.Size = new System.Drawing.Size(24, 26);
+            this.MinuteLabel1.TabIndex = 3;
+            this.MinuteLabel1.Text = "0";
+            // 
+            // MinuteLabel2
+            // 
+            this.MinuteLabel2.AutoSize = true;
+            this.MinuteLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinuteLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinuteLabel2.Location = new System.Drawing.Point(126, 9);
+            this.MinuteLabel2.Name = "MinuteLabel2";
+            this.MinuteLabel2.Size = new System.Drawing.Size(24, 26);
+            this.MinuteLabel2.TabIndex = 4;
+            this.MinuteLabel2.Text = "0";
+            // 
+            // SharerLabel2
+            // 
+            this.SharerLabel2.AutoSize = true;
+            this.SharerLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SharerLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SharerLabel2.Location = new System.Drawing.Point(156, 9);
+            this.SharerLabel2.Name = "SharerLabel2";
+            this.SharerLabel2.Size = new System.Drawing.Size(18, 26);
+            this.SharerLabel2.TabIndex = 8;
+            this.SharerLabel2.Text = ":";
+            // 
+            // SecondLabel1
+            // 
+            this.SecondLabel1.AutoSize = true;
+            this.SecondLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SecondLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SecondLabel1.Location = new System.Drawing.Point(180, 9);
+            this.SecondLabel1.Name = "SecondLabel1";
+            this.SecondLabel1.Size = new System.Drawing.Size(24, 26);
+            this.SecondLabel1.TabIndex = 5;
+            this.SecondLabel1.Text = "0";
+            // 
+            // SecondLabel2
+            // 
+            this.SecondLabel2.AutoSize = true;
+            this.SecondLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SecondLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SecondLabel2.Location = new System.Drawing.Point(210, 9);
+            this.SecondLabel2.Name = "SecondLabel2";
+            this.SecondLabel2.Size = new System.Drawing.Size(24, 26);
+            this.SecondLabel2.TabIndex = 6;
+            this.SecondLabel2.Text = "0";
+            // 
+            // WatchTimer
+            // 
+            this.WatchTimer.Enabled = true;
+            this.WatchTimer.Interval = 1000;
+            this.WatchTimer.Tick += new System.EventHandler(this.WatchTimer_Tick);
+            // 
+            // Background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 46);
+            this.Controls.Add(this.SecondLabel2);
+            this.Controls.Add(this.SecondLabel1);
+            this.Controls.Add(this.SharerLabel2);
+            this.Controls.Add(this.MinuteLabel2);
+            this.Controls.Add(this.MinuteLabel1);
+            this.Controls.Add(this.SharerLabel1);
+            this.Controls.Add(this.HourLabel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HourLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Background";
+            this.Text = "Watch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HourLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label HourLabel2;
+        private System.Windows.Forms.Label SharerLabel1;
+        private System.Windows.Forms.Label MinuteLabel1;
+        private System.Windows.Forms.Label MinuteLabel2;
+        private System.Windows.Forms.Label SharerLabel2;
+        private System.Windows.Forms.Label SecondLabel1;
+        private System.Windows.Forms.Label SecondLabel2;
+        private System.Windows.Forms.Timer WatchTimer;
+    }
+}
+
---
+++ HW/HW_7/HW_7.2/Watch/Watch/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Watch
+{
+    /// <summary>
+    /// The background of the app
+    /// </summary>
+    public partial class Background : Form
+    {
+        /// <summary>
+        /// Set current time to watch
+        /// </summary>
+        private void SetCurrentTime()
+        {
+            var date = DateTime.Now;
+
+            var hourNumber = date.Hour;
+            HourLabel1.Text = Convert.ToString(hourNumber / 10);
+            HourLabel2.Text = Convert.ToString(hourNumber % 10);
+            
+            var minuteNumber = date.Minute;
+            MinuteLabel1.Text = Convert.ToString(minuteNumber / 10);
+            MinuteLabel2.Text = Convert.ToString(minuteNumber % 10);
+
+            var secondNumber = date.Second;
+            SecondLabel1.Text = Convert.ToString(secondNumber / 10);
+            SecondLabel2.Text = Convert.ToString(secondNumber % 10);
+        }
+
+        /// <summary>
+        /// Constructor: initialize forms and current time
+        /// </summary>
+        public Background()
+        {
+            InitializeComponent();
+            SetCurrentTime();
+        }
+
+        /// <summary>
+        /// Data type for every watch element
+        /// </summary>
+        enum TimeLabel { HOUR1, HOUR2, MIN1, MIN2, SEC1, SEC2 };
+
+        /// <summary>
+        /// Check if digit is in corrent range as for time
+        /// </summary>
+        /// <param name="digit">The checking digit</param>
+        /// <param name="high">The value incorrect digits begin</param>
+        private void CheckIfDigitInRange(int digit, int minOutDigit = 10)
+        {
+            if (digit < 0 || digit >= minOutDigit)
+            {
+                throw new DataMisalignedException();
+            }
+        }
+
+        /// <summary>
+        /// Add time to watch
+        /// </summary>
+        /// <param name="timeLabel">The element of watch to be increased</param>
+        private void AddTime(TimeLabel timeLabel = TimeLabel.SEC2)
+        {
+            int digit = -1;
+            switch (timeLabel)
+            {
+                case TimeLabel.SEC2:
+                    digit = Convert.ToInt32(SecondLabel2.Text);
+                    CheckIfDigitInRange(digit, 10);
+
+                    ++digit;
+                    SecondLabel2.Text = Convert.ToString(digit % 10);
+                    if (digit >= 10)
+                    {
+                        AddTime(TimeLabel.SEC1);
+                    }
+                    break;
+
+                case TimeLabel.SEC1:
+                    digit = Convert.ToInt32(SecondLabel1.Text);
+                    CheckIfDigitInRange(digit, 6);
+
+                    ++digit;
+                    SecondLabel1.Text = Convert.ToString(digit % 6);
+                    if (digit >= 6)
+                    {
+                        AddTime(TimeLabel.MIN2);
+                    }
+                    break;
+
+                case TimeLabel.MIN2:
+                    digit = Convert.ToInt32(MinuteLabel2.Text);
+                    CheckIfDigitInRange(digit, 10);
+
+                    ++digit;
+                    MinuteLabel2.Text = Convert.ToString(digit % 10);
+                    if (digit >= 10)
+                    {
+                        AddTime(TimeLabel.MIN1);
+                    }
+                    break;
+
+                case TimeLabel.MIN1:
+                    digit = Convert.ToInt32(MinuteLabel1.Text);
+                    CheckIfDigitInRange(digit, 6);
+
+                    ++digit;
+                    MinuteLabel1.Text = Convert.ToString(digit % 6);
+                    if (digit >= 6)
+                    {
+                        AddTime(TimeLabel.HOUR2);
+                    }
+                    break;
+
+                case TimeLabel.HOUR2:
+                    digit = Convert.ToInt32(HourLabel2.Text);
+                    CheckIfDigitInRange(digit, 10);
+
+                    ++digit;
+                    HourLabel2.Text = Convert.ToString(digit % 10);
+                    if (digit >= 10 || HourLabel1.Text == "2" && digit >= 4)
+                    {
+                        AddTime(TimeLabel.HOUR1);
+                    }
+                    break;
+
+                case TimeLabel.HOUR1:
+                    digit = Convert.ToInt32(HourLabel1.Text);
+                    CheckIfDigitInRange(digit, 3);
+
+                    ++digit;
+                    HourLabel1.Text = Convert.ToString(digit % 3);
+                    if (digit >= 3)
+                    {
+                        HourLabel2.Text = "0";
+                        HourLabel1.Text = "0";
+                    }
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Timer action for every second
+        /// </summary>
+        private void WatchTimer_Tick(object sender, EventArgs e)
+        {
+            AddTime();
+        }
+
+    }
+
+}
