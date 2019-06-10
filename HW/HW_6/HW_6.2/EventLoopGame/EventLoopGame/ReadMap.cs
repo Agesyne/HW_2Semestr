0abb1476b0ca1ff4fc32d52975c454425ada7ef4
Agesyne <A40gE20Low8@mail.ru>
2019-06-10 16:29:37 +0300

    Tmp HW_6.2

---
+++ HW/HW_6/HW_6.2/EventLoopGame/EventLoopGame/ReadMap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EventLoopGame
+{
+    public static class ReadMap
+    {
+        public static string[,] ReadMap(string fileName)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(fileName);
+
+                int counter = 0;
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
